43d1cfda9f957351463d6b1b28ec8bf42ddfe03f
Ak-Yass <75218991+Ak-Yass@users.noreply.github.com>
2021-12-27 13:55:36 +0100

    Update st_covid.py

--- st_covid.py
+++ st_covid.py
@@ -28,8 +28,8 @@ st.markdown("""
 fas_data = keras.datasets.fashion_mnist
 (train_images, train_labels), (test_images, test_labels) = fas_data.load_data()
 
-cnn_model = load_model(".\test\cnn_model")
-# cnn_model = load_model("cnn_models")
+# cnn_model = load_model(".\test\cnn_model")
+cnn_model = load_model("cnn_models")
 
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
