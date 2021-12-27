9bd50df0171f6179be28e4392d4776cd82b2e2f9
Ak-Yass <75218991+Ak-Yass@users.noreply.github.com>
2021-12-27 14:00:46 +0100

    Update st_covid.py

--- st_covid.py
+++ st_covid.py
@@ -43,10 +43,10 @@ if add_selectbox == 'Welcome':
     st.title("WELCOME TO Fashion MNIST")
 
 
-    def file_selector(folder_path='.'):
-        filenames = os.listdir(folder_path)
-        selected_filename = st.selectbox('Select a file', filenames)
-        return os.path.join(folder_path, selected_filename)
+#     def file_selector(folder_path='.'):
+#         filenames = os.listdir(folder_path)
+#         selected_filename = st.selectbox('Select a file', filenames)
+#         return os.path.join(folder_path, selected_filename)
 
 
     filename = file_selector()
